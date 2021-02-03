bc3834e2d686f91a7cec5f4475ef786f032bb40b
Brenton Niebauer <b.niebauer@me.com>
2021-02-03 11:32:33 -0500

    Initial Commit

---
+++ Shared/SushiGoCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SushiGoCalculatorApp.swift
+//  Shared
+//
+//  Created by Brenton Niebauer on 2/3/21.
+//
+
+import SwiftUI
+
+@main
+struct SushiGoCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
